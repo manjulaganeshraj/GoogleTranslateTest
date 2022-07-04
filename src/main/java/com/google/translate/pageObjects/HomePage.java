e813b7988a0ca4110a13004bc985e3dfac9bc4ed
Manjula <manjulaganeshraj@gmail.com>
2022-07-04 12:44:01 -0700

    first commit

---
+++ src/main/java/com/google/translate/pageObjects/HomePage.java
@@ -0,0 +1,135 @@
+package com.google.translate.pageObjects;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import com.google.translate.page.BasePage;
+
+public class HomePage extends BasePage {
+
+	@FindBy(css = ".OlSOob > c-wiz[role='main'] .kHGNJd  .EO28P > div:nth-of-type(2) > .DiOXab.KY3GZb.VfPpkd-Bz112c-LgbsSe.VfPpkd-Bz112c-LgbsSe-OWXEXe-e5LLRc-SxQuSe.eT1oJ.qiN4Vb.szLmtb.yHy1rc > .VfPpkd-Bz112c-RLmnJb")
+	private WebElement sourceLangDrpdwn;
+
+	@FindBy(css = ".OlSOob > c-wiz[role='main'] .kHGNJd  .bvzp8c > .OoYv6d > .X4hZJc.pEyuac > .MeCBDd.dykxn.j33Gae > .vSUSRc > .F29iQc >.qSb8Pe >.Llmcnf")
+	private List<WebElement> sourceLangauages;
+
+	@FindBy(css = ".OlSOob > c-wiz[role='main'] .kHGNJd  .EO28P > div:nth-of-type(5) > .DiOXab.KY3GZb.VfPpkd-Bz112c-LgbsSe.VfPpkd-Bz112c-LgbsSe-OWXEXe-e5LLRc-SxQuSe.eT1oJ.qiN4Vb.szLmtb.yHy1rc")
+	private WebElement translateLangDrpdwn;
+
+	@FindBy(css = ".DlHcnf.bvzp8c > .ykTHSe > .X4hZJc.pEyuac .F29iQc > .qSb8Pe > .Llmcnf")
+	private List<WebElement> translateLangauages;
+
+	@FindBy(css = ".QFw9Te > textarea[role='combobox']")
+	private WebElement sourceText;
+
+	@FindBy(xpath = "//span[@class='Q4iAWc']")
+	private WebElement translatedPrimaryOutput;
+
+	@FindBy(css = ".oJweqc > .v3U01d")
+	private List<WebElement> translatedAlternateValues;
+
+	@FindBy(css = "[jsdata='deferred-i6'] [jscontroller='HwavCb'] .VfPpkd-Bz112c-RLmnJb")
+	private WebElement swapLangButton;
+
+	@FindBy(css = ".DiOXab.GA2I6e.VfPpkd-Bz112c-LgbsSe.VfPpkd-Bz112c-LgbsSe-OWXEXe-e5LLRc-SxQuSe.eT1oJ.qiN4Vb.yHy1rc > .VfPpkd-Bz112c-RLmnJb")
+	private WebElement clearSourceText;
+
+	@FindBy(css = "a:nth-of-type(2) > .ita-kd-arrow.ita-kd-icon-span.ita-kd-img")
+	private WebElement selectInputTool;
+
+	@FindBy(xpath = "//*[@id=\"yDmH0d\"]/ul/li[1]/span[3]")
+	private WebElement screenKeyboard;
+
+	@FindBy(xpath = "//*[@id=\"K72\"]")
+	private WebElement screenKeyboardLetterH;
+
+	@FindBy(xpath = "//*[@id=\"K73\"]")
+	private WebElement screenKeyboardLetterI;
+
+	@FindBy(xpath = "//*[@id=\"K49\"]")
+	private WebElement screenKeyboardLetterExclamation;
+
+	@FindBy(css = ".vk-btn:nth-of-type(1) .vk-sf-c16")
+	private WebElement screenKeyboardToggleCase;
+
+	@FindBy(css = ".swFHJd")
+	private WebElement translatedTextInput;
+
+	private  String siteUrl = null;
+
+	public HomePage(WebDriver driver, String siteUrl) {
+		super(driver);
+		this.siteUrl = siteUrl;
+	}
+
+	public void load(){
+		driver.get(siteUrl);
+	}
+
+	public void clickSourceLangDrpdwn(){
+		sourceLangDrpdwn.click();
+
+	}
+
+	public void selectSourceLangDrpdwn(String sourceLang) throws InterruptedException{
+		for (WebElement element : sourceLangauages) {
+			if(element.getText().equalsIgnoreCase(sourceLang)) {
+				element.click();
+				Thread.sleep(1000);
+				break;
+			}
+		}
+	}
+
+	public void clickTranslateLangDrpdwn(){
+		translateLangDrpdwn.click();
+
+	}
+
+	public WebElement getTranslatedOutputElement(){
+		return translatedPrimaryOutput;
+
+	}
+
+	public List<WebElement> getTranslatedAlternateValues(){
+		return translatedAlternateValues;
+
+	}
+
+	public void selectTranslateLangDrpdwn(String translateLang) throws InterruptedException {
+		for (WebElement element : translateLangauages) {
+			if(element.getText().equalsIgnoreCase(translateLang)) {
+				element.click();
+				break;
+			}
+		}
+	}
+
+	public void enterInitialText(String initialText) throws InterruptedException{
+		sourceText.sendKeys(initialText);
+
+	}
+
+	public void clickswapLangButton() throws InterruptedException{
+		swapLangButton.click();
+		Thread.sleep(2000); 
+
+	}
+
+	public void enterUsingScreenKeyboard() throws InterruptedException{
+
+		clearSourceText.click();
+		selectInputTool.click();
+		screenKeyboard.click();
+		screenKeyboardToggleCase.click();
+		screenKeyboardLetterH.click();
+		screenKeyboardLetterI.click();
+		screenKeyboardToggleCase.click();
+		screenKeyboardLetterExclamation.click();
+
+	}
+
+
+}
---
+++ src/test/java/com/test/webPages/GoogleTranslateAssignmentTest.java
@@ -0,0 +1,104 @@
+package com.test.webPages;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.google.translate.pageObjects.HomePage;
+import com.test.base.TestBase;
+
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
+public class GoogleTranslateAssignmentTest extends TestBase {
+
+	@Test
+	public void firstTest() throws IOException, InterruptedException, ParseException {
+
+		//Open https://translate.google.com/ website
+		HomePage homePage = new HomePage(driver, prop.getProperty("url"));
+		homePage.load();
+		
+		//parse JSON test inputs with UTF encoding
+		JSONObject input = parseTestInput();
+		String sourceLang = (String)input.get("sourceLang");
+		String destinationLang = (String)input.get("destinationLang");
+		String sourceText = (String)input.get("sourceText");
+		String expectedTranslation = (String)input.get("expectedTranslation");
+		
+		//select source language from the drop-down menu on the left 
+		homePage.clickSourceLangDrpdwn();
+		homePage.selectSourceLangDrpdwn(sourceLang);
+		
+		//select translation language from the drop-down menu on the right 
+		homePage.clickTranslateLangDrpdwn();
+		homePage.selectTranslateLangDrpdwn(destinationLang);
+		
+		//Enter text to translate
+		homePage.enterInitialText(sourceText);
+		//validate
+		Boolean translationResult = validateTranslation(homePage, sourceText, expectedTranslation);
+		Assert.assertTrue(translationResult, "Couldnot find the expected result after translation.");
+		
+		//SCENARIO2 - "click swap languages button and verify the result."
+		homePage.clickswapLangButton();
+		Boolean swapTranslationResult = validateTranslation(homePage, expectedTranslation, sourceText);
+		Assert.assertTrue(swapTranslationResult, "Couldnot find the expected result after swap.");
+		
+		//SCENARIO3 - "clear the input field, click "select input tool" button, select "screen keyboard" and  enter "Hi!" "
+		homePage.enterUsingScreenKeyboard();
+		
+	}
+	
+	private JSONObject parseTestInput() throws IOException, ParseException {
+		//JSON parser object to parse read file
+        JSONParser jsonParser = new JSONParser();
+        JSONObject textInput = null;
+        //with UTF encoding for special alphabets on some languages
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+        		new FileInputStream("./src/test/resources/testInput.json"), "UTF-8"));
+        
+        //Read JSON file
+        Object obj = jsonParser.parse(reader);
+        textInput = (JSONObject) obj;
+         
+		return textInput;
+	}
+	
+	private Boolean validateTranslation(HomePage homePage, String inputText, String expectedText) throws InterruptedException{
+		
+		WebElement translatedOutputElement = homePage.getTranslatedOutputElement();
+		String resultText = translatedOutputElement.getText();
+		
+		//Validate with primary display text
+		if(expectedText.equalsIgnoreCase(resultText)) {
+			return true;
+		}
+		
+		//If expected is not in primary display text, try alternate values as per Google Translate by clicking result
+		translatedOutputElement.click();
+		Thread.sleep(1000);
+		
+		for (WebElement element : homePage.getTranslatedAlternateValues()){
+			
+			WebElement sourceInResult = element.findElement(By.className("swFHJd"));
+			WebElement translatedTextInResult = element.findElement(By.className("NqnNQd"));
+			
+			if(sourceInResult.getText().equalsIgnoreCase(inputText)) {
+				if(translatedTextInResult.getText().equalsIgnoreCase(expectedText)) {
+					translatedTextInResult.click();
+					return true;
+				}
+			}
+		}
+		
+		translatedOutputElement.click();
+		return false;
+	}
+}
